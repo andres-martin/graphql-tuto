6bb76f3c48c1e06a58a5251dbd754cad131e706b
andres-martin <andresmartin266@gmail.com>
2020-01-26 19:21:26 -0500

    [add] add recursive' calls

--- server.js
+++ server.js
@@ -13,12 +13,21 @@ const typeDefs = `
     url: String!
     description: String!
     author: User!
+    comments: [Comment!]
   }
   
   type User {
     id: Int! @unique
     username: String!
     about: String!
+  }
+
+  type Comment {
+    id: Int! @unique
+    parent: Comment
+    comments: [Comment]
+    author: User!
+    content: String!
   }
 
   type Query {
@@ -39,6 +48,14 @@ const users = [
   { id: 1, username: 'user2', about: 'The second user' },
 ];
 
+const commentsList = [
+  { id: 0, parent: 0, author: 0, content: 'Comment 1' },
+  { id: 1, parent: 0, author: 1, content: 'Comment 2' },
+  { id: 2, parent: 1, author: 0, content: 'Comment 3' },
+  { id: 3, parent: 0, author: 2, content: 'Comment 4' },
+  { id: 4, parent: 1, author: 2, content: 'Comment 5' },
+];
+
 const resolvers = {
   Query: {
     allLinks: () => links,
@@ -48,6 +65,11 @@ const resolvers = {
   },
   Link: {
     author: ({ author }) => find(users, { id: author }),
+    comments: ({ comments }) => comments.map(i => find(commentsList, { id: i })),
+  },
+  Comment: {
+    author: ({ author }) => find(users, { id: author }),
+    comments: () => {},
   },
 };
 
